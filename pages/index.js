3577c56d70884864b8c6a4cbb502a85c3cc2cd06
Arvind Singh <er.arvind.negi@gmail.com>
2020-09-27 20:09:26 +0300

    next apollo example code

--- pages/index.js
+++ pages/index.js
@@ -14,52 +14,16 @@ export default function Home() {
         <h1 className={styles.title}>My experiments with Technology</h1>
 
         <div className={styles.grid}>
-          <Link href="/cv">
-            <a>CV</a>
+          <Link href="/bus-schedule">
+            <a>My Next Bus</a>
+          </Link>
+          <Link href="/tree-map">
+            <a>Tree Map</a>
           </Link>
         </div>
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
-      <footer className={styles.footer}>
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a> */}
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
